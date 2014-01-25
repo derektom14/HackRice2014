7b70ed80d9802a57ade11750d8e1bebe4156733a
Charbunny <char_bunny@hotmail.com>
2014-01-25 14:02:42 -0600

    Updates to IAssignment

--- HWOrganizer/src/hwo/IAssignment.java
+++ HWOrganizer/src/hwo/IAssignment.java
@@ -13,20 +13,19 @@ public interface IAssignment
 	public String getAssignmentLoc();
 	public String getTurninLoc();
 	public String getResources();
-	public String getAssignemntType();
+	public String getAssignmentType();
 	public String getHWName();
 	public String getNotes();
 	public Duration getCompletionTime();
 	public Date getStartDate();
 	public Date getEndDate();
-	public boolean[] getFrequency();
 	
 	// Set Methods
 	public void setCourse(Course c);
 	public void setAssignmentLoc(String s);
 	public void setTurninLoc(String s);
 	public void setResources(String s);
-	public void setAssignemntType(String s);
+	public void setAssignmentType(String s);
 	public void setHWName(String s);
 	public void setNotes(String s);
 	public void setCompletionTime(Duration d);
@@ -34,8 +33,6 @@ public interface IAssignment
 	public void setStartDate(Date d);
 	// -d must be later than the current end date
 	public void setEndDate(Date d);
-	// - b must have a length of 7
-	public void setFrequency(boolean[] b);
 	
 
 }
