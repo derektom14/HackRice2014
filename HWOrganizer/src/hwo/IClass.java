77910cf7c0db55ffa89a7959aaa254acc9b07ff9
mongoose700 <msp700@gmail.com>
2014-01-25 12:48:35 -0600

    Added Class and Semester Interfaces.

---
+++ HWOrganizer/src/hwo/IClass.java
@@ -0,0 +1,16 @@
+// Author: Michael
+
+package hwo;
+
+public interface IClass {
+	public String getName();
+	public String getAssignmentLocation();
+	public String getTurnInLocation();
+	public String getResources();
+	public String getAssignmentType();
+	public void setName();
+	public void setAssignmentLocation();
+	public void setTurnInLocation();
+	public void setResources();
+	public void setAssignmentType();
+}
--- HWOrganizer/src/hwo/ISemester.java
+++ HWOrganizer/src/hwo/ISemester.java
@@ -1,7 +1,11 @@
+// Author: Michael
+
 package hwo;
 import java.util.Date;
 
 public interface ISemester {
 	public Date getStartDate();
 	public Date getEndDate();
+	public void setStartDate(Date startDate);
+	public void setEndDate(Date endDate);
 }
