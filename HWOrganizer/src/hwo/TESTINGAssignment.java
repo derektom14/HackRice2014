161b74da69690a44b1f54a9442e75778119ae189
Charbunny <char_bunny@hotmail.com>
2014-01-25 16:18:44 -0600

    Updated unused import in Course

--- HWOrganizer/src/hwo/TESTINGAssignment.java
+++ HWOrganizer/src/hwo/TESTINGAssignment.java
@@ -3,8 +3,12 @@
 // Has various tests for assignments
 
 package hwo;
+import java.io.*;
 
 public class TESTINGAssignment 
 {
-
+	public static void main (String [] args) throws IOException
+	{
+		
+	}
 }
