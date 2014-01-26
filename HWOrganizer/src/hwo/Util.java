43c7d596c65e7a8f7dcfc2defb5db3441c920e0e
mongoose700 <msp700@gmail.com>
2014-01-25 22:46:44 -0600

    Added methods to open URL's and Files using a default browser or the default program for a type of file.

--- HWOrganizer/src/hwo/Util.java
+++ HWOrganizer/src/hwo/Util.java
@@ -2,9 +2,16 @@ package hwo;
 
 import java.awt.Component;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
+
+import java.awt.Desktop;
+import java.net.URI;
 
 public class Util {
 
@@ -17,6 +24,54 @@ public class Util {
 			File file = chooser.getSelectedFile();
 			System.out.println(file);
 			field.setText(file.getAbsolutePath());
+		}
+	}
+	
+	public static boolean isURL(String text) {
+		try {
+			URL u = new URL(text);
+			u.toURI();
+			return true;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static boolean isFile(String text) {
+		File file = new File(text);
+		return file.exists();
+	}
+	
+	public static int testText(String text) {
+		if (isURL(text))
+			return 1;
+		if (isFile(text))
+			return 2;
+		return 0;
+	}
+	
+	public static void openURL(String text) {
+		try {
+			URI uri = new URI(text);
+			Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+		            desktop.browse(uri);
+		    }
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void openFile(String text) {
+		try {
+			Desktop.getDesktop().open(new File(text));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
