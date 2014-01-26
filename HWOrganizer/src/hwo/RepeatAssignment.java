97f9a49cde601806702ecb9da420d139c0b1da88
Charbunny <char_bunny@hotmail.com>
2014-01-26 01:07:29 -0600

    Added print statements for debugging

--- HWOrganizer/src/hwo/RepeatAssignment.java
+++ HWOrganizer/src/hwo/RepeatAssignment.java
@@ -109,6 +109,7 @@ public class RepeatAssignment extends AAssignment
 	// Creates SingleAssignments - used by full constructor
 	public void createSingleAssignments()
 	{
+		System.out.println("CteatingSingleAssignment");
 		// Convert Calendar to a changeable format
 		GregorianCalendar g = new GregorianCalendar(getStartDate().get(Calendar.YEAR),
 				getStartDate().get(Calendar.MONTH), getStartDate().get(Calendar.DAY_OF_MONTH));
@@ -121,6 +122,7 @@ public class RepeatAssignment extends AAssignment
 			{
 				int numWeeks = (weekCounter % 7) + 1;
 				if ((numWeeks + 1) % this.frequency == 0){
+					System.out.println("New Assignment: " + (g.get(Calendar.DAY_OF_WEEK)- 1) + " " + weekCounter);
 					this.assignments.add(new SingleAssignment(this, new GregorianCalendar(g.get(Calendar.YEAR), g.get(Calendar.MONTH), g.get(Calendar.DAY_OF_MONTH))));
 				}
 			}
