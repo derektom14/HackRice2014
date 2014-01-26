9fc130f3ccdc3ada27554505c12df22da8892b37
Charbunny <char_bunny@hotmail.com>
2014-01-25 18:24:41 -0600

    Commenting and code style clean-ups

--- HWOrganizer/src/hwo/IAssignment.java
+++ HWOrganizer/src/hwo/IAssignment.java
@@ -13,36 +13,36 @@ public interface IAssignment
 	//-------------------------------------------------
 	// Getter Methods
 	//-------------------------------------------------
+	public String getName();
 	public String getNotes();
 	public Duration getCompletionTime();
 	public Course getCourse();
 	// Can be from course
-	public String getName();
 	public String getAssignmentLoc();
 	public String getTurninLoc();
 	public String getResources();
 	public String getAssignmentType();
 	public Calendar getDueTime();
-	// Can be from semester
+	// Can be from course / semester
 	public Calendar getStartDate();
 	public Calendar getEndDate();
 	
 	//-------------------------------------------------
 	// Setter Methods
 	//-------------------------------------------------
-	public void setNotes(String s);
-	public void setCompletionTime(Duration d);
-	public void setCourse(Course c);
+	public void setName(String name);
+	public void setNotes(String notes);
+	public void setCompletionTime(Duration time);
+	public void setCourse(Course course);
 	// Can override value from course
-	public void setName(String s);
-	public void setAssignmentLoc(String s);
-	public void setTurninLoc(String s);
-	public void setResources(String s);
-	public void setAssignmentType(String s);
-	public void setDueTime(Calendar t);
-	// Can override value from semester
-	public void setStartDate(Calendar d);
-	public void setEndDate(Calendar d);
+	public void setAssignmentLoc(String location);
+	public void setTurninLoc(String location);
+	public void setResources(String resources);
+	public void setAssignmentType(String type);
+	public void setDueTime(Calendar time);
+	// Can override value from course / semester
+	public void setStartDate(Calendar date);
+	public void setEndDate(Calendar date);
 	
 
 }
