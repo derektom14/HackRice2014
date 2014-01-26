5ce68edf7bb3ec42d28932ae3d2f0864eac87fdd
mongoose700 <msp700@gmail.com>
2014-01-25 23:11:21 -0600

    Settings now uses ICourse rather than Course.

--- HWOrganizer/src/hwo/Settings.java
+++ HWOrganizer/src/hwo/Settings.java
@@ -4,23 +4,23 @@ package hwo;
 import java.util.Calendar;
 
 public class Settings {
-	private Course course;
+	private ICourse course;
 	private Calendar startDate;
 	private Calendar endDate;
 	
-	Settings (Course course, Calendar startDate, Calendar endDate) {
+	Settings (ICourse course, Calendar startDate, Calendar endDate) {
 		this.course = course;
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
 	
 	// Accessor methods
-	public Course getCourse(){return course;}
+	public ICourse getCourse(){return course;}
 	public Calendar getStartDate(){return startDate;}
 	public Calendar getEndDate(){return endDate;}
 	
 	// Setter methods
-	public void setCourse(Course course){this.course = course;}
+	public void setCourse(ICourse course){this.course = course;}
 	public void setStartDate(Calendar startDate){this.startDate = startDate;}
 	public void setEndDate(Calendar endDate){this.endDate = endDate;}
 }
