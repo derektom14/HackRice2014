d5cc034e4fb4ef84c15485e2c9b377a98aa289ce
Charbunny <char_bunny@hotmail.com>
2014-01-26 00:48:06 -0600

    Flipped comparator

--- HWOrganizer/src/hwo/AssignmentDateComparator.java
+++ HWOrganizer/src/hwo/AssignmentDateComparator.java
@@ -6,9 +6,9 @@ public class AssignmentDateComparator extends AssignmentComparator {
 
 	public int compare(SingleAssignment a, SingleAssignment b) {
 		if (a.getDueDate().after(b.getDueDate()))
-			return -1;
-		if (a.getDueDate().before(b.getDueDate()))
 			return 1;
+		if (a.getDueDate().before(b.getDueDate()))
+			return -1;
 		return 0;
 	}
 }
